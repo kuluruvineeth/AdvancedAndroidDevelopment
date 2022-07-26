730e274c390d2158c173c00866f57d7b25d0dc25
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-26 15:54:14 +0530

    normal way of data binding.

--- app/src/main/java/com/kuluruvineeth/databindingwithobjects/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/databindingwithobjects/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kuluruvineeth.databindingwithobjects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kuluruvineeth.databindingwithobjects.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val student = getStudent()
+        binding.apply {
+            nameText.text = student.name
+            emailText.text = student.email
+        }
+    }
+    private fun getStudent():Student{
+        return Student(1,"Vineeth","Vineeth@gmail.com")
     }
 }
