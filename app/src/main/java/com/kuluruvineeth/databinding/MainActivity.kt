f6092d67df0b69e91f9a50bb6c31240e1ca0e1e5
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-26 14:53:28 +0530

    normal method - findViewById

--- app/src/main/java/com/kuluruvineeth/databinding/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/databinding/MainActivity.kt
@@ -2,10 +2,25 @@ package com.kuluruvineeth.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.submit_button)
+        button.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    private fun displayGreeting(){
+        val messageView = findViewById<TextView>(R.id.greeting_text_view)
+        val nameText = findViewById<EditText>(R.id.name_edit_text)
+
+        val message = "hello! " + nameText.text
+        messageView.text = message
     }
 }
