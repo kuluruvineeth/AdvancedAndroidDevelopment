3130c1030b616018a3de30ec0060e62eb19a7daa
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-26 17:00:03 +0530

    without viewModel on rotation count resets.

--- app/src/main/java/com/kuluruvineeth/viewmodel/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/viewmodel/MainActivity.kt
@@ -2,10 +2,19 @@ package com.kuluruvineeth.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kuluruvineeth.viewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.countText.text = count.toString()
+        binding.button.setOnClickListener {
+            count++
+            binding.countText.text = count.toString()
+        }
     }
 }
