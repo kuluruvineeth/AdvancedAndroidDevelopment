2601c9ee288ee205cb0676136e31cf607156a470
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-26 17:21:39 +0530

    with viewModel on rotation, count persists.

--- app/src/main/java/com/kuluruvineeth/viewmodel/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/viewmodel/MainActivity.kt
@@ -3,18 +3,19 @@ package com.kuluruvineeth.viewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.kuluruvineeth.viewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var count = 0
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        binding.countText.text = count.toString()
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.countText.text = viewModel.getCurrentCount().toString()
         binding.button.setOnClickListener {
-            count++
-            binding.countText.text = count.toString()
+            binding.countText.text = viewModel.getUpdatedCount().toString()
         }
     }
 }
