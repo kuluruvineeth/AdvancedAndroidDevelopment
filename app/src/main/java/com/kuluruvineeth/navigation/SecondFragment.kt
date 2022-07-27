7bd8ba1c5ab438b356fc5795720a45f3f607f94b
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-27 15:13:28 +0530

    setup navigation destinations.

---
+++ app/src/main/java/com/kuluruvineeth/navigation/SecondFragment.kt
@@ -0,0 +1,22 @@
+package com.kuluruvineeth.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.kuluruvineeth.navigation.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second,container,false)
+        return binding.root
+    }
+
+}
