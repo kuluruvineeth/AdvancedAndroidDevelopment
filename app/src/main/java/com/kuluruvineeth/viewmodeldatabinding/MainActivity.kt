e0b13a952ded1c0e17c2531eec44651ba1a40e2d
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-27 09:05:41 +0530

    normal(indirect) way of data binding with viewModel.

--- app/src/main/java/com/kuluruvineeth/viewmodeldatabinding/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/viewmodeldatabinding/MainActivity.kt
@@ -2,10 +2,25 @@ package com.kuluruvineeth.viewmodeldatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.kuluruvineeth.viewmodeldatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        viewModel.count.observe(this, Observer {
+            binding.countText.text = it.toString()
+        })
+
+        binding.button.setOnClickListener {
+            viewModel.updateCount()
+        }
     }
 }
