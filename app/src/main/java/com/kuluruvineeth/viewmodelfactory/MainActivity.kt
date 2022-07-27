063c1560fbbc1f4aad00d1ba5289b9651d456f42
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-27 07:14:13 +0530

    using normal subclass of viewModel.

--- app/src/main/java/com/kuluruvineeth/viewmodelfactory/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/viewmodelfactory/MainActivity.kt
@@ -2,10 +2,22 @@ package com.kuluruvineeth.viewmodelfactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.kuluruvineeth.viewmodelfactory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.resultTextView.text = viewModel.getTotal().toString()
+
+        binding.insertButton.setOnClickListener {
+            viewModel.setTotal(binding.inputEditText.text.toString().toInt())
+            binding.resultTextView.text = viewModel.getTotal().toString()
+        }
     }
 }
