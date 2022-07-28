d96a2e3d78292beadd83b6be3aed458c38ff8208
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-28 06:59:09 +0530

    navigation actions - moving from one fragment to another.

--- app/src/main/java/com/kuluruvineeth/navigation/HomeFragment.kt
+++ app/src/main/java/com/kuluruvineeth/navigation/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.kuluruvineeth.navigation.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,6 +17,9 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
         return binding.root
     }
 }
