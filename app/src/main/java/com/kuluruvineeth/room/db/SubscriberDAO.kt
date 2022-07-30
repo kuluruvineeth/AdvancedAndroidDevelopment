b04de10a3540b65fd86474cc9ab50cd21a345a03
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-30 09:56:56 +0530

    created subscriber DAO interface.

---
+++ app/src/main/java/com/kuluruvineeth/room/db/SubscriberDAO.kt
@@ -0,0 +1,22 @@
+package com.kuluruvineeth.room.db
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SubscriberDAO {
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber)
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscribers():Flow<List<Subscriber>>
+}
