e64fcb93bee5bb3ee707a21a3ab863300119fbe2
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-30 16:54:04 +0530

    user input validations.

--- app/src/main/java/com/kuluruvineeth/room/SubscriberViewModel.kt
+++ app/src/main/java/com/kuluruvineeth/room/SubscriberViewModel.kt
@@ -1,5 +1,6 @@
 package com.kuluruvineeth.room
 
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -32,16 +33,24 @@ class SubscriberViewModel(
     }
 
     fun saveOrUpdate(){
-        if(isUpdateOrDelete){
-            subscriberToUpdateOrDelete.name = inputName.value!!
-            subscriberToUpdateOrDelete.email = inputEmail.value!!
-            update(subscriberToUpdateOrDelete)
+        if(inputName.value==null){
+            statusMessage.value = Event("Please enter subscriber's name")
+        }else if(inputEmail.value==null){
+            statusMessage.value = Event("Please enter subscriber's email")
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(inputEmail.value!!).matches()){
+            statusMessage.value = Event("Please enter correct email address")
         }else{
-            val name = inputName.value!!
-            val email = inputEmail.value!!
-            insert(Subscriber(0,name,email))
-            inputName.value = null
-            inputEmail.value = null
+            if(isUpdateOrDelete){
+                subscriberToUpdateOrDelete.name = inputName.value!!
+                subscriberToUpdateOrDelete.email = inputEmail.value!!
+                update(subscriberToUpdateOrDelete)
+            }else{
+                val name = inputName.value!!
+                val email = inputEmail.value!!
+                insert(Subscriber(0,name,email))
+                inputName.value = null
+                inputEmail.value = null
+            }
         }
     }
 
