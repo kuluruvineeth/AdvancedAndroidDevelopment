41aafcffa500eeb4247cc72029a9a095a625100b
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-30 08:20:14 +0530

    running in main thread

--- app/src/main/java/com/kuluruvineeth/lifecyclescope/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/lifecyclescope/MainActivity.kt
@@ -2,17 +2,30 @@ package com.kuluruvineeth.lifecyclescope
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.lifecycle.lifecycleScope
 import com.kuluruvineeth.lifecyclescope.ui.main.MainFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progressBar)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+
+        lifecycleScope.launch {
+            delay(5000)
+            progressBar.visibility = View.VISIBLE
+            delay(10000)
+            progressBar.visibility = View.GONE
         }
     }
 }
