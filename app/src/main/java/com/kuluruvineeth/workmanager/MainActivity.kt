ef9932251ed4c82ac923a94f1392732b33a2cac6
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-31 11:13:49 +0530

    added code to get status updates from workers.

--- app/src/main/java/com/kuluruvineeth/workmanager/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/workmanager/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kuluruvineeth.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
@@ -16,9 +17,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun setOneTimeWorkRequest(){
+        val workManager = WorkManager.getInstance(applicationContext)
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .build()
-        WorkManager.getInstance(applicationContext)
-            .enqueue(uploadRequest)
+        workManager.enqueue(uploadRequest)
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id)
+            .observe(this, Observer {
+                textView.text = it.state.name
+            })
     }
 }
