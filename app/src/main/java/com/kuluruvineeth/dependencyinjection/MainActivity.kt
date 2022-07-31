bc9ca9a463cb1e110c8f43a042090df6850c38cd
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-31 15:03:06 +0530

    field injection with Dagger2

--- app/src/main/java/com/kuluruvineeth/dependencyinjection/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/dependencyinjection/MainActivity.kt
@@ -2,20 +2,16 @@ package com.kuluruvineeth.dependencyinjection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var smartPhone: SmartPhone
+    @Inject lateinit var smartPhone: SmartPhone
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*val smartPhone = SmartPhone(
-            Battery(),
-            SIMCard(ServiceProvider()),
-            MemoryCard()
-        ).makeACallWithRecording()*/
         DaggerSmartPhoneComponent.create()
-            .getSmartPhone()
-            .makeACallWithRecording()
+            .inject(this)
+        smartPhone.makeACallWithRecording()
     }
 }
--- app/src/main/java/com/kuluruvineeth/dependencyinjection/SmartPhoneComponent.kt
+++ app/src/main/java/com/kuluruvineeth/dependencyinjection/SmartPhoneComponent.kt
@@ -5,5 +5,5 @@ import dagger.Component
 @Component(modules = [MemoryCardModule::class,NCBatteryModule::class])
 interface SmartPhoneComponent {
 
-    fun getSmartPhone() : SmartPhone
+    fun inject(mainActivity: MainActivity)
 }
