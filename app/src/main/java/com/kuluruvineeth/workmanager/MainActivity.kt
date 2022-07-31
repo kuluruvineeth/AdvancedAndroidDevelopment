f4f2ffa7f1640ba69b2f432623b12add761a79a7
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-31 12:07:25 +0530

    set input and output data of worker class.

--- app/src/main/java/com/kuluruvineeth/workmanager/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/workmanager/MainActivity.kt
@@ -2,11 +2,16 @@ package com.kuluruvineeth.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val KEY_COUNT_VALUE = "key_count"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,17 +21,27 @@ class MainActivity : AppCompatActivity() {
     }
     private fun setOneTimeWorkRequest(){
         val workManager = WorkManager.getInstance(applicationContext)
+
+        val data:Data = Data.Builder()
+            .putInt(KEY_COUNT_VALUE,125)
+            .build()
         val constraints = Constraints.Builder()
             .setRequiresCharging(true)
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .setConstraints(constraints)
+            .setInputData(data)
             .build()
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
                 textView.text = it.state.name
+                if(it.state.isFinished){
+                    val data = it.outputData
+                    val message = data.getString(UploadWorker.KEY_WORKER)
+                    Toast.makeText(applicationContext,message,Toast.LENGTH_LONG).show()
+                }
             })
     }
 }
