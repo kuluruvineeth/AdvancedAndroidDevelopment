288fc0f07d33fe991631ba0357c69832d91cc1ea
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-31 09:38:18 +0530

    added tap action for notification.

--- app/src/main/java/com/kuluruvineeth/notifications/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/notifications/MainActivity.kt
@@ -2,7 +2,9 @@ package com.kuluruvineeth.notifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -24,12 +26,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayNotification(){
         val notificationId = 45
+        val tapResultIntent = Intent(this,SecondActivity::class.java)
+        val pendingIntent : PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_MUTABLE
+        )
         val notification = NotificationCompat.Builder(this@MainActivity,channelID)
             .setContentTitle("AgriRize")
             .setContentText("This is to take you into the world of Agririze")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager?.notify(notificationId,notification)
     }
