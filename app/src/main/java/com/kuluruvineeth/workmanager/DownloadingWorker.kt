cfb7832a08d46784fde09cce7043d94abd95ed56
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-31 13:35:30 +0530

    periodic work request

--- app/src/main/java/com/kuluruvineeth/workmanager/DownloadingWorker.kt
+++ app/src/main/java/com/kuluruvineeth/workmanager/DownloadingWorker.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DownloadingWorker(context: Context,params:WorkerParameters) : Worker(context,params) {
     override fun doWork(): Result {
@@ -11,6 +13,9 @@ class DownloadingWorker(context: Context,params:WorkerParameters) : Worker(conte
             for(i in 0..3000){
                 Log.i("MYTAG","Downloading $i")
             }
+            val time = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentDate = time.format(Date())
+            Log.i("MYTAG","Completed $currentDate")
             return Result.success()
         }catch (e:Exception){
             return Result.failure()
