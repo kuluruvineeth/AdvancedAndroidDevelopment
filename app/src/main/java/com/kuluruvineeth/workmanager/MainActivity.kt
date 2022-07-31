e4b727a5dde933e09a1622309bc9f45e51c74c7a
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-31 11:29:58 +0530

    set constraints on work manager.

--- app/src/main/java/com/kuluruvineeth/workmanager/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/workmanager/MainActivity.kt
@@ -3,9 +3,7 @@ package com.kuluruvineeth.workmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +16,12 @@ class MainActivity : AppCompatActivity() {
     }
     private fun setOneTimeWorkRequest(){
         val workManager = WorkManager.getInstance(applicationContext)
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
             .build()
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
