886d859a977e34d64b5fa44f37b67d8d3c767d2b
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-31 09:30:19 +0530

    created simple notification.

--- app/src/main/java/com/kuluruvineeth/notifications/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/notifications/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.kuluruvineeth.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val channelID = "com.kuluruvineeth.notifications.channel1"
+    private var notificationManager : NotificationManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelID,"AgriRizeChannel","This is to take you into the world of Agririze")
+        button.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun displayNotification(){
+        val notificationId = 45
+        val notification = NotificationCompat.Builder(this@MainActivity,channelID)
+            .setContentTitle("AgriRize")
+            .setContentText("This is to take you into the world of Agririze")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+        notificationManager?.notify(notificationId,notification)
+    }
+
+    private fun createNotificationChannel(id:String,name:String,channelDescription:String){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id,name,importance).apply {
+                description = channelDescription
+            }
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 }
