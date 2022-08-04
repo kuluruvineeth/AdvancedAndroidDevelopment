f7fe14cdeda850abd9cb06a5d0c93ac24f124f92
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-04 10:08:17 +0530

    created simple service example.

--- app/src/main/java/com/kuluruvineeth/service/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/service/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.kuluruvineeth.service
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kuluruvineeth.service.MyBackgroundService.Companion.MARKS
+import com.kuluruvineeth.service.MyBackgroundService.Companion.NAME
+import com.kuluruvineeth.service.MyBackgroundService.Companion.TAG
+import com.kuluruvineeth.service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val serviceIntent = Intent(this,MyBackgroundService::class.java)
+        serviceIntent.putExtra(NAME,"Vineeth")
+        serviceIntent.putExtra(MARKS,100)
+        binding.btnStart.setOnClickListener {
+            Log.i(TAG,"Started service")
+            startService(serviceIntent)
+        }
+
+        binding.btnStop.setOnClickListener {
+            Log.i(TAG,"Stopping service")
+            stopService(serviceIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/kuluruvineeth/service/MyBackgroundService.kt
@@ -0,0 +1,36 @@
+package com.kuluruvineeth.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyBackgroundService : Service() {
+
+    init {
+        Log.i(TAG,"Service has been created")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(TAG,"Service Started")
+        val name = intent?.getStringExtra(NAME)
+        val marks = intent?.getIntExtra(MARKS,0)
+        Log.i(TAG,"name is $name and marks are $marks")
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        Log.i(TAG,"Destroying....")
+        super.onDestroy()
+    }
+
+    companion object{
+        const val TAG = "MYTAG"
+        const val NAME = "NAME"
+        const val MARKS = "MARKS"
+    }
+}
